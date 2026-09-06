d0ae0757081518e9041756d527fbb0329330e41a
ikramblm <ikramblmdh7@gmail.com>
2026-09-06 23:11:17 +0100

    Make list-row photo thumbnails bigger (44px -> 64px)

--- lib/screens/entity_list_screen.dart
+++ lib/screens/entity_list_screen.dart
@@ -47,14 +47,14 @@ class _EntityListScreenState extends State<EntityListScreen> {
     Widget placeholder() => Container(
           color: Colors.grey.shade200,
           alignment: Alignment.center,
-          child: Icon(Icons.image_outlined, color: Colors.grey.shade400, size: 20),
+          child: Icon(Icons.image_outlined, color: Colors.grey.shade400, size: 28),
         );
 
     return ClipRRect(
-      borderRadius: BorderRadius.circular(8),
+      borderRadius: BorderRadius.circular(10),
       child: SizedBox(
-        width: 44,
-        height: 44,
+        width: 64,
+        height: 64,
         child: (path != null && path.isNotEmpty)
             ? Image.file(File(path), fit: BoxFit.cover, errorBuilder: (_, __, ___) => placeholder())
             : placeholder(),
